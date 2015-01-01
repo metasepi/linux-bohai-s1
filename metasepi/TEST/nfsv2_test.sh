60718b08cb8d6e2a6041ed999cca0676a0ef73ee
Kiwamu Okabe <kiwamu@debian.or.jp>
2015-01-01 15:51:31 +0900

    Run dbench on test

--- metasepi/TEST/nfsv2_test.sh
+++ metasepi/TEST/nfsv2_test.sh
@@ -1,20 +1,31 @@
 #! /bin/sh
 
+EXPORT_PATH=/var/exports/1
+MNT_PATH=./mnt/1_nfs2
+DBENCH_NPROCS=5
+DBENCH_TIMELIMIT=60
+
 oneTimeSetUp()
 {
-    sudo umount -f ./mnt/1_nfs2 > /dev/null 2>&1
-    sudo mount -t nfs -o nfsvers=2 localhost:/var/exports/1 ./mnt/1_nfs2
-    rm -rf ./mnt/1_nfs2/*
+    sudo umount -f ${MNT_PATH} > /dev/null 2>&1
+    sudo mount -t nfs -o nfsvers=2 localhost:${EXPORT_PATH} ${MNT_PATH}
+    rm -rf ${MNT_PATH}/*
 }
 
 oneTimeTearDown()
 {
-    sudo umount -f ./mnt/1_nfs2
+    sudo umount -f ${MNT_PATH}
 }
 
 testShowmount()
 {
-    showmount localhost -e | grep /var/exports/1
+    showmount localhost -e | grep ${EXPORT_PATH}
+    assertTrue $?
+}
+
+testDbench()
+{
+    dbench -D ${MNT_PATH} -t ${DBENCH_TIMELIMIT} ${DBENCH_NPROCS}
     assertTrue $?
 }
 
